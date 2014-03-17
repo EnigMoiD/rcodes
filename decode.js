a4b7cbcbd5cf4af9431d389993565abe39521bf5
EnigMoiD <evan.dorsky@icloud.com>
2014-03-17 18:28:07 -0400

    New schema, detect more errors

--- decode.js
+++ decode.js
@@ -1,37 +1,46 @@
 function decode(code) {
 	try {
 		if (code.length < 3 || code.length > 5)
-			throw "Invalid resistor code length."
+			throw "Invalid number of bands."
+		var map = {
+			k: { mant: 0, exp: 0, tol: null, temp: null },
+			b: { mant: 1, exp: 1, tol: .01, temp: 100 },
+			r: { mant: 2, exp: 2, tol: .02, temp: 50 },
+			o: { mant: 3, exp: 3, tol: null, temp: 15 },
+			y: { mant: 4, exp: 4, tol: null, temp: 25 },
+			g: { mant: 5, exp: 5, tol: null, temp: .5 },
+			u: { mant: 6, exp: 6, tol: null, temp: .25 },
+			v: { mant: 7, exp: 7, tol: null, temp: .1 },
+			a: { mant: 8, exp: 8, tol: null, temp: null },
+			i: { mant: 9, exp: 9, tol: null, temp: null },
+			s: { mant: null, exp: -2, tol: .1, temp: null },
+			l: { mant: null, exp: -1, tol: .05, temp: null }
+		}
+		var bands = code.length;
+
+		var tol = map[code[bands-1]].tol
+		if (tol === null)
+			throw "Invalid tolerance band."
+
+		var mult = Math.pow(10, map[code[bands-2]].exp)
+
+		var value = 0
+		var mantissa = bands-2
+		for (var i = 0; i < mantissa; i++) {
+			var num = map[code[i]].mant
+			if (num === null)
+				throw "Invalid mantissa band."
+			value += num * Math.pow(10, mantissa-i-1)
+		}
 	}
 	catch (e) {
 		return "Error: " + e
 	}
-	var map = {
-		k: 0,
-		b: 1,
-		r: 2,
-		o: 3,
-		y: 4,
-		g: 5,
-		u: 6,
-		v: 7,
-		a: 8,
-		i: 9
-	}
-	var bands = code.length;
 
-	var tol = code[bands-1]
-
-	var mult = Math.pow(10, map[code[bands-2]])
-
-	var value = 0
-	var mantissa = bands-2
-	for (var i = 0; i < mantissa; i++) {
-		var num = map[code[i]]
-		value += num*Math.pow(10, mantissa-i-1)
-	}
-
-	return value*mult;
+	return {
+		value: value*mult,
+		tolerance: tol
+	};
 }
 
 module.exports.decode = decode;
--- spec/decodespec.js
+++ spec/decodespec.js
@@ -2,14 +2,18 @@ var decode = require('../decode.js').decode
 
 describe("The resistor decoder", function() {
 	it("decodes 4-band resistors.", function() {
-		expect(decode("rogr")).toBe(2300000);
+		expect(decode("rogr").value).toBe(2300000);
 	})
 	
 	it("decodes 5-band resistors.", function() {
-		expect(decode("bkkkr")).toBe(100);
+		expect(decode("bkkkr").value).toBe(100);
 	})
 	
-	it("doesn't decode 6-band resistors.", function() {
-		expect(decode("bkkkru")).toBe('Error: Invalid resistor code length.');
+	it("doesn't decode 6-band resistors (yet).", function() {
+		expect(decode("bkkkru")).toBe('Error: Invalid number of bands.');
+	})
+	
+	it("detects invalid mantissa bands.", function() {
+		expect(decode("slbr")).toBe('Error: Invalid mantissa band.');
 	})
 })
